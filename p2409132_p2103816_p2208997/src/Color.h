f16d900385fac148c538078aff79a4ba2d1d15eb
p2409132 <aissatou.barry@etu.univ-lyon1.fr>
2025-03-18 09:07:03 +0100

    .......

---
+++ p2409132_p2103816_p2208997/src/Color.h
@@ -0,0 +1,40 @@
+#ifndef COLOR_H
+#define COLOR_H
+#include <iostream>
+
+/**
+ * @struct Color
+ * @brief Couleur de chaque joueur
+ */
+struct Color{
+    unsigned char r, v,b; ///< composante rouge vert bleu de la couleur 
+
+    /**
+     * @brief constructeur par defaut de la classe Color
+     */
+    Color();
+
+    /**
+     * @brief destructeur de la classe Color
+     */
+    ~Color();
+
+    /**
+     * @brief constructeur/changer  par initialisation de la classe
+     * @param nr la couleur rouge
+     * @param nv la couleur verte
+     * @param nb la couleur bleue
+     */
+    void setColor(unsigned char nr, unsigned char nv, unsigned char nb); 
+
+    /**
+     * @brief methode pour recuperer la couleur
+     */
+    const Color& getColor();
+
+
+}
+
+
+
+#endif
