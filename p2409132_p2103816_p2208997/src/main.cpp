37854df2c2890d216d915d538a596365c25c9856
BOUHNAIDA FARAH p2208997 <p2208997@b761pc0214.univ-lyon1.fr>
2025-03-18 09:52:38 +0100

    modif joueur

--- p2409132_p2103816_p2208997/src/main.cpp
+++ p2409132_p2103816_p2208997/src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() 
 {
     int id = 42;
-    Joueur j1(id);
+    Joueur j1(id,0,0,0);
     cout << "Joueur créé avec ID " << id << ". Nombre de pions arrivés: " << j1.getNbpionArrives() << endl;
     cout << "Le nombre de pions arrivés du joueur1 est: " << j1.getNbpionArrives() << endl;
     j1.getNbpionArrives();
