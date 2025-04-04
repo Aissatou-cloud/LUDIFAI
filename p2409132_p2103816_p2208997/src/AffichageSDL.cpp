39de9cae9711fbd24d45f451dfee5734ea7e19bb
p2409132 <aissatou.barry@etu.univ-lyon1.fr>
2025-04-04 11:32:07 +0200

    ....

--- p2409132_p2103816_p2208997/src/AffichageSDL.cpp
+++ p2409132_p2103816_p2208997/src/AffichageSDL.cpp
@@ -1,1 +1,25 @@
-#include "AffichageSDL.cpp"
+#include "AffichageSDL.h"
+#include <iostream>
+using namespace std;
+
+Affich
+
+
+
+
+void AffichageSDL:: SdlAff(){
+    //remplir l'ecran de blanc
+    //SDL_SetRenderDrawColor: definit la couleur de fond du rendu
+    SDL_SetRenderDrawColor(m_renderer,255, 255, 255, 255);
+
+    //efface l'ecran avec cette couleur pour ne pas supersposer 
+    //l'ancien rendu avec le nouveau
+    SDL_RenderClear(m_renderer);
+
+    /* A REVOIR POUR  m_font_im.draw*/
+
+}
+
+
+
+
